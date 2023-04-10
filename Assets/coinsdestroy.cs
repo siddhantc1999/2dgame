be3cf6dcf5560c9a5209a1cd9fe7fac836d05d52
siddhantc1999 <schiring369@gmail.com>
2023-04-10 16:49:30 +0530

    2d game

---
+++ Assets/coinsdestroy.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class coinsdestroy : MonoBehaviour
+{
+    public bool destroy;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+
+    }
+    public void autodestroy()
+    {
+        if (destroy)
+        {
+            for (int i = 0; i < transform.childCount; i++)
+            {
+                Debug.Log(transform.childCount);
+                Destroy(transform.GetChild(i));
+            }
+        }
+    }
+}
